5177d6c0bfdb63d75dd94343dc7649ad57c96d2b
daniel <daniel>
2025-09-02 18:06:41 +0100

    add hdl_proj folder to .gitignore

---
+++ Prototyping/Minimal-RAMFS/setup_env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#----------------------------------------
+# setup_env.sh
+# Adding Xilinx Tool Paths
+#----------------------------------------
+
+export PATH="/tools/Xilinx/Vivado/2024.1/bin:$PATH"
+export PATH="/tools/Xilinx/Vitis/2024.1/bin:$PATH"
+
+source /tools/Xilinx/Petalinux/2024.1/settings.sh
+
+
